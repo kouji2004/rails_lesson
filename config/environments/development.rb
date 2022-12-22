48a2ccb4bddc8ff1ab84f2d33f64bd1ff11ad8e0
kouji2004 <mrtaxi48@gmail.com>
2022-12-22 10:04:02 +0000

    ブラウザに接続できる

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.hosts << "fa2239eb047b43b2b1d324e9e2ac3d52.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
