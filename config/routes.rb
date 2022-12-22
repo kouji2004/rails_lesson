8ae42d7cf30a491639786dd3b4520a689621c90c
kouji2004 <mrtaxi48@gmail.com>
2022-12-22 10:30:02 +0000

    新規登録画面の作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+devise_for :users,skip: [:passwords], controllers: {
+    registrations: "users/registrations",
+    sessions: 'users/sessions'
+  }
+
 end
